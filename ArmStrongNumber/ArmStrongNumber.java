fb3f0c9d8101bc561923b573a513dc8c2947744e
sohag-84 <injamulhaqsohag19977@gmail.com>
2023-07-29 11:07:17 +0600

    armstrong number check

---
+++ ArmStrongNumber/ArmStrongNumber.java
@@ -0,0 +1,25 @@
+package ArmStrongNumber;
+
+import java.util.Scanner;
+
+public class ArmStrongNumber {
+    public static void main(String[] args) {
+        System.out.print("Enter the number: ");
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int original = n;
+        int sum = 0;
+
+        while (n > 0) {
+            int rem = n % 10;
+            int cube = (rem * rem * rem);
+            sum += cube;
+            n /= 10;
+        }
+        if (sum == original) {
+            System.out.println("Number is armstrong number");
+        } else {
+            System.out.println("Number is not armstrong number");
+        }
+    }
+}
