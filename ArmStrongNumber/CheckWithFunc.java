fbb14f860df365139d78fd04cbc24faf72ff2149
sohag-84 <injamulhaqsohag19977@gmail.com>
2023-07-29 11:17:53 +0600

    armstrong number check with function

---
+++ ArmStrongNumber/CheckWithFunc.java
@@ -0,0 +1,30 @@
+package ArmStrongNumber;
+
+import java.util.Scanner;
+
+public class CheckWithFunc {
+    public static void main(String[] args) {
+        try (Scanner sc = new Scanner(System.in)) {
+            int n = sc.nextInt();
+
+            System.out.println(isArmstrongNumber(n));
+        }
+    }
+
+    static boolean isArmstrongNumber(int n) {
+        int original = n;
+        int sum = 0;
+        while (n > 0) {
+            int rem = n % 10;
+            int cube = rem * rem * rem;
+            sum += cube;
+            n /= 10;
+        }
+        return sum == original;
+        // if (sum == original) {
+        // return true;
+        // } else {
+        // return false;
+        // }
+    }
+}
