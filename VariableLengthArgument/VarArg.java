6b55584e9479b76f4216a97d498347677912e92d
sohag-84 <injamulhaqsohag19977@gmail.com>
2023-07-29 10:47:34 +0600

    Block scope

--- VariableLengthArgument/VarArg.java
+++ VariableLengthArgument/VarArg.java
@@ -4,10 +4,10 @@ import java.util.Arrays;
 
 public class VarArg {
     public static void main(String[] args) {
-        addition(12,34,5,6);
+        addition(12, 34, 5, 6);
     }
-    
-    //infinite parameter list
+
+    // infinite parameter list
     static void addition(int... v) {
         System.out.println(Arrays.toString(v));
     }
