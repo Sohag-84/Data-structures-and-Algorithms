78b4a378196e7fa7b823bde0974b567d23a03535
Sohag <ihsohag-84@gmail.com>
2023-08-14 11:31:39 +0600

    find smallest number and sort the array

---
+++ ArrayExample/SmallestNumber.java
@@ -0,0 +1,17 @@
+package ArrayExample;
+
+public class SmallestNumber {
+
+    public static void main(String[] args) {
+        int arr[] = { 102, 32, 4, 5, 60, 70 };
+        int min = arr[0];
+
+        for (int i = 0; i < arr.length; i++) {
+            if (min > arr[i]) {
+                min = arr[i];
+            }
+        }
+
+        System.out.println("Smallest Number: " + min);
+    }
+}
