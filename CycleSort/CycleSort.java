b167baf0f66a5895f009a82cd06739fca0ed05cc
Sohag-84 <ihsohag84@gmail.com>
2023-12-30 21:26:46 +0600

    cycle sort 0-N array element sorting

--- CycleSort/CycleSort.java
+++ CycleSort/CycleSort.java
@@ -4,18 +4,39 @@ import java.util.Arrays;
 
 public class CycleSort {
     public static void main(String[] args) {
-        // =>array element must be 1-N number. otherwise it doesn't not work
-        int arr[] = { 2, 5, 1, 4, 3 };
-        sort(arr);
-        System.out.println(Arrays.toString(arr));
+        // =>array element must be 1-N number. otherwise it doesn't not work.
+        /// for 1-N array element sort
+        int arr1[] = { 2, 5, 1, 4, 3 };
+        sort(arr1);
+        System.out.println("1-N array element sort: " + Arrays.toString(arr1));
+
+        // =>array element must be 0-N number. otherwise it doesn't not work.
+        /// for 0-N array element sort
+        int arr[] = { 4, 2, 3, 1, 0 };
+        sort2(arr);
+        System.out.println("0-N array element sort: " + Arrays.toString(arr));
     }
 
+    /// for 1-N array sort
     static void sort(int arr[]) {
         int i = 0;
         while (i < arr.length) {
             int correctPosition = arr[i] - 1;
             if (arr[i] != arr[correctPosition]) {
                 swap(arr, i, correctPosition);
+            } else {
+                i++;
+            }
+        }
+    }
+
+    /// for 0-N array sort
+    static void sort2(int arr[]) {
+        int i = 0;
+        while (i < arr.length) {
+            int correct = arr[i];
+            if (arr[i] != arr[correct]) {
+                swap(arr, i, correct);
             } else {
                 i++;
             }
