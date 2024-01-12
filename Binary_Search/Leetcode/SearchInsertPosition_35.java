69501762cff30e43e5f7f1c2e5e30aa2a30d165f
Sohag-84 <ihsohag84@gmail.com>
2024-01-12 12:16:10 +0600

    leetcode 35 number (search insert position) problem is solved

---
+++ Binary_Search/Leetcode/SearchInsertPosition_35.java
@@ -0,0 +1,29 @@
+package Binary_Search.Leetcode;
+
+//https://leetcode.com/problems/search-insert-position/description/
+
+public class SearchInsertPosition_35 {
+    public static void main(String[] args) {
+        int arr[] = { 1, 3, 5,6 };
+        int target = 2;
+        int postion = searchInsert(arr, target);
+        System.out.println("Insert position: " + postion);
+    }
+
+    static int searchInsert(int[] nums, int target) {
+        int start = 0;
+        int end = nums.length - 1;
+        while (start <= end) {
+            int mid = start + (end - start) / 2;
+            if (nums[mid] == target) {
+                return mid;
+            }
+            if(nums[mid]<target){
+                start = mid+1;
+            }else{
+                end = mid-1;
+            }
+        }
+        return start;
+    }
+}
