ccdb4a6907bcc342b5aac20599acce11ffcb2150
Sohag-84 <ihsohag84@gmail.com>
2024-01-12 19:37:16 +0600

    leetcode 645 number (set mismatch problem is solved) is solved

--- Binary_Search/Leetcode/SearchInsertPosition_35.java
+++ Binary_Search/Leetcode/SearchInsertPosition_35.java
@@ -4,7 +4,7 @@ package Binary_Search.Leetcode;
 
 public class SearchInsertPosition_35 {
     public static void main(String[] args) {
-        int arr[] = { 1, 3, 5,6 };
+        int arr[] = { 1, 3, 5, 6 };
         int target = 2;
         int postion = searchInsert(arr, target);
         System.out.println("Insert position: " + postion);
@@ -18,10 +18,10 @@ public class SearchInsertPosition_35 {
             if (nums[mid] == target) {
                 return mid;
             }
-            if(nums[mid]<target){
-                start = mid+1;
-            }else{
-                end = mid-1;
+            if (nums[mid] < target) {
+                start = mid + 1;
+            } else {
+                end = mid - 1;
             }
         }
         return start;
---
+++ CycleSort/Leetcode/SetMismatch_645.java
@@ -0,0 +1,40 @@
+package CycleSort.Leetcode;
+
+import java.util.Arrays;
+
+public class SetMismatch_645 {
+    public static void main(String[] args) {
+        int nums[] = { 1, 1 };
+        System.out.println("Result: " + Arrays.toString(findErrorNums(nums)));
+    }
+
+    static int[] findErrorNums(int[] nums) {
+        int arr[] = new int[2];
+        int i = 0;
+        while (i < nums.length) {
+            int correctPosition = nums[i] - 1;
+            if (nums[i] != nums[correctPosition]) {
+                swap(nums, i, correctPosition);
+            } else {
+                i++;
+            }
+        }
+
+        for (int index = 0; index < nums.length; index++) {
+            if (index + 1 != nums[index]) {
+                for (int j = 0; j < arr.length - 1; j++) {
+                    arr[j] = nums[index];
+                    arr[j + 1] = index + 1;
+                }
+            }
+        }
+
+        return arr;
+    }
+
+    static void swap(int nums[], int first, int second) {
+        int temp = nums[first];
+        nums[first] = nums[second];
+        nums[second] = temp;
+    }
+}
