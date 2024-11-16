d63af4ed5dc21fde110392bb1b9fec7cd623fbaf
Sohag-84 <ihsohag84@gmail.com>
2024-11-17 00:02:34 +0600

    made calculator program

---
+++ Practice_Problem/Calculator.java
@@ -0,0 +1,56 @@
+package Practice_Problem;
+
+import java.util.Scanner;
+
+public class Calculator {
+    public static void main(String[] args) {
+        try (Scanner sc = new Scanner(System.in)) {
+            int result = 0;
+
+            /// take input from user till doesn't press 'X' or 'x'
+            while (true) {
+                System.out.print("Enter operator: ");
+                char op = sc.next().trim().charAt(0);
+                if (op == '+' || op == '-' || op == '*' || op == '/' || op == '%') {
+                    // take input two number
+                    System.out.print("Enter two number: ");
+                    int num1 = sc.nextInt();
+                    int num2 = sc.nextInt();
+
+                    // perform operation
+                    if (op == '+') {
+                        result = num1 + num2;
+                    }
+                    if (op == '-') {
+                        result = num1 - num2;
+                    }
+                    if (op == '*') {
+                        result = num1 * num2;
+                    }
+                    if (op == '/') {
+                        if (num2 != 0) {
+                            result = num1 / num2;
+                        } else {
+                            System.out.println("Answer: Infinity\n");
+                        }
+                    }
+                    if (op == '%') {
+                        result = num1 % num2;
+                    }
+
+                    if (op != '/' || num2 != 0) {
+                        System.out.println("Answer: " + result + "\n");
+                    }
+                }
+
+                else if (op == 'x' || op == 'X') {
+                    System.out.println("Programm closed");
+                    break;
+                } else {
+                    System.out.println("Invalid operation\n\n");
+                }
+            }
+        }
+
+    }
+}
