9c8b37cded2411446f8f948e43620c68758c4f02
Sohag-84 <ihsohag84@gmail.com>
2024-12-17 18:25:09 +0600

    leetcode 189 number problem solved

---
+++ ArrayExample/Leetcode/RotateArray_189.java
@@ -0,0 +1,29 @@
+package ArrayExample.Leetcode;
+
+import java.util.Arrays;
+
+public class RotateArray_189 {
+    public static void main(String[] args) {
+        int[] arr = { 1, 2, 3, 4, 5, 6, 7 };
+        int k = 3;
+        rotate(arr, k);
+
+        System.out.println(Arrays.toString(arr));
+    }
+
+    static void rotate(int[] nums, int k) {
+        int n = nums.length;
+        k = k % n;
+        int[] ans = new int[n];
+
+        for (int i = n - k; i < n; i++) {
+            ans[i - (n - k)] = nums[i];
+        }
+        for (int i = k; i < n; i++) {
+            ans[i] = nums[i - k];
+        }
+        for (int i = 0; i < n; i++) {
+            nums[i] = ans[i];
+        }
+    }
+}
