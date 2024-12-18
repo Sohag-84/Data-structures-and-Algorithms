9ecf6131add3e85aee41270ad9d47729bbb94e8b
Sohag-84 <ihsohag84@gmail.com>
2024-12-18 18:39:10 +0600

    solved leetcode 136 number problem

---
+++ ArrayExample/Leetcode/SingleNumber_136.java
@@ -0,0 +1,17 @@
+package ArrayExample.Leetcode;
+
+public class SingleNumber_136 {
+    public static void main(String[] args) {
+        int[] nums = { 4, 1, 2, 1, 2 };
+        System.out.println(singleNumber(nums));
+    }
+
+    // hint-> user XOR operator to solve this problem
+    static int singleNumber(int[] nums) {
+        int result = 0;
+        for (int i = 0; i < nums.length; i++) {
+            result = result ^ nums[i];
+        }
+        return result;
+    }
+}
