804c54a09708b96ce90ddc347b210984d9f32e43
Sohag-84 <ihsohag84@gmail.com>
2024-12-18 17:19:56 +0600

    solved leetcode 27 number problem

--- ArrayExample/Leetcode/CheckStringPangram_1832.java
+++ ArrayExample/Leetcode/CheckStringPangram_1832.java
@@ -1,18 +1,20 @@
 package ArrayExample.Leetcode;
 
+// https://leetcode.com/problems/check-if-the-sentence-is-pangram/description/
+
 public class CheckStringPangram_1832 {
     public static void main(String[] args) {
         System.out.println(checkIfPangram("hdz"));
     }
 
     static boolean checkIfPangram(String sentence) {
-        if(sentence.length()<26){
+        if (sentence.length() < 26) {
             return false;
-         }
+        }
         for (char i = 'a'; i <= 'z'; i++) {
-            if (sentence.indexOf(i)<0) {
+            if (sentence.indexOf(i) < 0) {
                 return false;
-            } 
+            }
         }
         return true;
     }
--- ArrayExample/Leetcode/RemoveDuplicatesValue_26.java
+++ ArrayExample/Leetcode/RemoveDuplicatesValue_26.java
@@ -1,5 +1,7 @@
 package ArrayExample.Leetcode;
 
+//https://leetcode.com/problems/remove-duplicates-from-sorted-array/description/
+
 public class RemoveDuplicatesValue_26 {
     public static void main(String[] args) {
         int[] nums = { 0, 0, 1, 1, 1, 2, 2, 3, 3, 4 };
