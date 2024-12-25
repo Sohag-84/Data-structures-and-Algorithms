e52ea6b95c45c2dc33446adf6b15dd1cf8529305
Sohag-84 <ihsohag84@gmail.com>
2024-12-26 00:16:35 +0600

    move zeros problem optimal solution added

--- Array/Leetcode/MoveZeros_283.java
+++ Array/Leetcode/MoveZeros_283.java
@@ -2,6 +2,8 @@ package Array.Leetcode;
 
 //https://leetcode.com/problems/move-zeroes/description/
 
+// https://www.naukri.com/code360/problems/ninja-and-the-zero-s_6581958?leftPanelTabValue=PROBLEM
+
 import java.util.*;
 
 public class MoveZeros_283 {
@@ -13,7 +15,31 @@ public class MoveZeros_283 {
         System.out.println(Arrays.toString(nums));
     }
 
+    // optimal solution
+    // using 2 pointer
     static void moveZeroes(int[] nums) {
+        int j = -1;
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] == 0) {
+                j = i;
+                break;
+            }
+        }
+        if (j == -1) {
+            return;
+        }
+        for (int i = j; i < nums.length; i++) {
+            if (nums[i] != 0) {
+                int temp = nums[i];
+                nums[i] = nums[j];
+                nums[j] = temp;
+                j++;
+            }
+        }
+    }
+
+    /// brute force approace
+    static void moveZeroes2(int[] nums) {
         ArrayList<Integer> temp = new ArrayList<Integer>();
 
         for (int i = 0; i < nums.length; i++) {
