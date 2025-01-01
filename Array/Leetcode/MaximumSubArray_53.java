4ecc313136e64fde3ce58eb52ff1870558c44633
Sohag-84 <ihsohag84@gmail.com>
2025-01-01 17:23:22 +0600

    solved leetcode 53 number problem

---
+++ Array/Leetcode/MaximumSubArray_53.java
@@ -0,0 +1,24 @@
+package Array.Leetcode;
+// https://leetcode.com/problems/maximum-subarray/submissions/1493961666/
+
+public class MaximumSubArray_53 {
+    public static void main(String[] args) {
+        int[] nums = { -2, 1, -3, 4, -1, 2, 1, -5, 4 };
+        System.out.println(maxSubArray(nums));
+    }
+
+    static int maxSubArray(int[] nums) {
+        int max = Integer.MIN_VALUE;
+        int sum = 0;
+        for (int i = 0; i < nums.length; i++) {
+            sum += nums[i];
+            if (sum > max) {
+                max = sum;
+            }
+            if (sum < 0) {
+                sum = 0;
+            }
+        }
+        return max;
+    }
+}
