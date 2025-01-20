34ba9e5c3473942bfd52b7c06c36d7c0123354da
Sohag-84 <ihsohag84@gmail.com>
2025-01-21 00:12:23 +0600

    upper bound search in binary search is added

--- Binary_Search/Problem_solve/UpperBound.java
+++ Binary_Search/Problem_solve/UpperBound.java
@@ -1,25 +1,24 @@
 package Binary_Search.Problem_solve;
 
-// https://www.geeksforgeeks.org/problems/floor-in-a-sorted-array-1587115620/1?track=DSASP-Searching&amp%253BbatchId=154    
+//https://www.naukri.com/code360/problems/implement-upper-bound_8165383?leftPanelTabValue=SUBMISSION
 
-public class LowerBound_2 {
+public class UpperBound {
     public static void main(String[] args) {
         int[] arr = { 1, 2, 3, 6, 7, 8, 9 };
-        System.out.println(lowerBoundSearch(arr, 5));
+        System.out.println(upperBoundSearch(arr, 10));
     }
 
-    static int lowerBoundSearch(int[] arr, int target) {
+    static int upperBoundSearch(int[] arr, int target) {
         int start = 0;
         int end = arr.length - 1;
-        int ans = -1;
+        int ans = arr.length;
         while (start <= end) {
             int mid = start + (end - start) / 2;
-            if (arr[mid] <= target) {
+            if (arr[mid] > target) {
                 ans = mid;
-                start = mid + 1;
-            } else {
                 end = mid - 1;
-
+            } else {
+                start = mid + 1;
             }
         }
         return ans;
